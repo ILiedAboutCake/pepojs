fb3e7e3c71eb5137f55a1b8c492ddfc80318875b
ILiedAboutCake <iliedaboutthecake@gmail.com>
2021-10-24 22:01:43 +0000

    add frog getting to legacy command

--- events/message.js
+++ events/message.js
@@ -1,15 +1,25 @@
+const { MessageAttachment } = require('discord.js');
 const config = require('../config.json');
+const globalImagePool = require('../helpers/imagepool');
 
 module.exports = {
   name: 'messageCreate',
-  execute(message) {
+  async execute(message) {
     // no bots
     if (message.author.bot) return;
 
     // set legacyCommands in config.json with commands to redirect
     if (config.legacyCommands.some(cmd => message.content.startsWith(cmd))) {
       console.log(`[LEGACY COMMAND] ${message.author.id} in guild ${message.guild.id} sent ${message.content}`);
-      message.reply('Pepo now uses slash commands! Give `/pepo` a try');
+
+      const randomFrog = await globalImagePool.get();
+      const attachment = new MessageAttachment(`frogs/${randomFrog}`);
+
+      await message.reply(
+        {
+          content: 'After April 2022 slash commands are required by the Discord API. Give `/pepo` a try',
+          files: [attachment],
+        });
     }
   },
 };
