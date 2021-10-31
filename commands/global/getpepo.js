e55d435cb9925fc4122e9412c342f90e6e8c3738
ILiedAboutCake <iliedaboutthecake@gmail.com>
2021-10-31 21:40:42 +0000

    add getpepo command

---
+++ commands/global/getpepo.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const config = require('../../config.json');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('getpepo')
+    .setDescription('Want Pepo for your own server? Get my join url!'),
+  async execute(interaction) {
+    await interaction.reply({
+      content: `<https://discord.com/oauth2/authorize?client_id=${config.clientId}&scope=bot&permissions=274877942784>`,
+      ephemeral: true,
+    });
+  },
+};
