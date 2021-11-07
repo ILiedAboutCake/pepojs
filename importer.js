81b968a2056336900f423208d360ffe8a0728ec4
ILiedAboutCake <iliedaboutthecake@gmail.com>
2021-11-07 04:48:31 +0000

    offline importer

---
+++ importer.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const fs = require('fs');
+const crypto = require('crypto');
+
+// stolen logic from addfrog.js, will mass import blindly any files in _temp
+
+// hash + filename validation
+const reg = new RegExp('^[0-9a-f]{40}\\.(png|jpg|gif)$');
+
+const frogs = fs.readdirSync('./frogs/_temp');
+
+console.log(frogs.length);
+
+for (const tempPath of frogs) {
+
+  // pull the temp file from fs
+  const tempFileBuffer = fs.readFileSync(path.resolve(__dirname, 'frogs/_temp', tempPath));
+
+  // get sha1 hash
+  const hashSum = crypto.createHash('sha1');
+  hashSum.update(tempFileBuffer);
+  const frogHash = hashSum.digest('hex');
+  const frogExtension = path.extname(tempPath);
+  const frogFileName = `${frogHash}${frogExtension}`;
+
+  // validate the name of new frog
+  if (!reg.test(frogFileName)) {
+    console.log(`${frogFileName} failed validation`);
+  }
+  else {
+    const frogAcceptedPath = path.resolve(__dirname, 'frogs', frogFileName);
+    fs.writeFileSync(frogAcceptedPath, tempFileBuffer);
+  }
+
+
+}
