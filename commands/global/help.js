3cb6c8f58608c9f7f2a6e7f0d527a863dffc5118
ILiedAboutCake <iliedaboutthecake@gmail.com>
2021-11-19 02:04:55 +0000

    better help command

--- commands/global/help.js
+++ commands/global/help.js
@@ -1,31 +1,39 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const config = require('../../config.json');
 
+const supportText = `
+Frogbot 3 "Pepo". Built and maintained with love by Cake#0001 <https://cake.sh>
+
+**Mention Support:**
+Due to the Discord API changes, after April 2022 you can no longer use ! commands to access a bot.
+To work around this you must mention me <@${config.clientId}> to get a reply, OR use Discord slash commands.
+
+**Slash Commands:**
+\`/pepo\`: Post a frog
+\`/getpepo\`: Get the Discord invite URL for this bot
+\`/pingpepo\`: Check if Pepo is awake and feels like chatting :)
+\`/froglimit get\`: Get the rate limit or ignored status for the current channel
+
+**Moderation Commands:**
+\`/froglimit set 0, [${config.rateLimit.min} - ${config.rateLimit.max}]\` seconds
+Set to 0 to ignore the channel. Default ratelimit is ${config.rateLimit.default} seconds
+Setting ratelimit requires one of the following permissions: \`${config.frogmodFlags}\`
+
+Frogbot is designed with joy in mind. Suggestions for new images can be submitted in the Pepo support Discord.
+Additionally, if you believe a frog should be removed, please reach out.
+
+Looking for better moderation tools? Check out my other project, Bernard. <https://getbernard.io>
+
+**Support Server:** ${config.supportInvite}
+`;
+
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('pepohelp')
     .setDescription('Have a new frog to suggest? Think one should be removed? Can\'t remember the commands?'),
   async execute(ctx) {
     await ctx.interaction.reply({
-      content: `
-      Frogbot 3 "Pepo". Built and maintained with love by Cake#0001 <https://cake.sh>
-  
-      **The Basics:**
-      \`/pepo\`: Post a frog
-      \`/getpepo\`: Get the Discord invite URL for this bot
-      \`/pingpepo\`: Check if Pepo is awake and feels like chatting :)
-      \`/froglimit get\`: Get the rate limit or ignored status for the current channel
-  
-      **Moderation Commands:**
-      \`/froglimit set 0, [${config.rateLimit.min} - ${config.rateLimit.max}]\` seconds
-      Set to 0 to ignore the channel. Default ratelimit is ${config.rateLimit.default} seconds
-      Setting ratelimit requires one of the following permissions: \`${config.frogmodFlags}\`
-  
-      Frogbot is designed with joy in mind. Suggestions for new images can be submitted in the Pepo support Discord.
-      Additionally, if you believe a frog should be removed, please reach out.
-  
-      **Support Server:** ${config.supportInvite}
-      `,
+      content: supportText,
       ephemeral: true,
     });
   },
--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,6 @@ module.exports = {
   execute(client, baseLogger) {
     baseLogger.info(`Logged into Discord gateway as ${client.user.id}`);
 
-    client.user.setActivity('/pepo | /getpepo');
+    client.user.setActivity('@pepo | /pepo | /pepohelp');
   },
 };
